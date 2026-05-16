e38218c23f49d2cb0c9661a52fff003bf5cbd40d
Leticia-SS <leticia.saraiva013@gmail.com>
2026-05-16 19:10:05 -0300

    initial commit

---
+++ src/main/java/com/example/catalog/Monster/Catalog/System/MonsterCatalogSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.catalog.Monster.Catalog.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterCatalogSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsterCatalogSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/catalog/Monster/Catalog/System/MonsterCatalogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catalog.Monster.Catalog.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterCatalogSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
