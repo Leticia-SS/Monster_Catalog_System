82d89979b00640f9323a5bdc163f6b19d1a2309c
Leticia-SS <leticia.saraiva013@gmail.com>
2026-05-16 20:28:57 -0300

    create service for Region and Sighting

---
+++ src/main/java/com/example/catalog/Monster/Catalog/System/region/service/SightingService.java
@@ -0,0 +1,28 @@
+package com.example.catalog.Monster.Catalog.System.region.service;
+
+import com.example.catalog.Monster.Catalog.System.region.model.Sighting;
+import com.example.catalog.Monster.Catalog.System.region.repository.ISightingRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class SightingService {
+    private final ISightingRepository sightingRepository;
+
+    public List<Sighting> getAllByRegion(Long regionId) {
+        return sightingRepository.findByRegionId(regionId);
+    }
+
+    public List<Sighting> getAllByMonster(Long monsterId) {
+        return sightingRepository.findByMonsterId(monsterId);
+    }
+
+    //OBS: Terminar o register que faz chamada para o monster-service
+    //public Sighting registerSighting(Sighting sighting) {}
+
+
+
+}
